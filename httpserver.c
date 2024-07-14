4ce230f8d70ad455a378813abeda0beb0c3e689a
James Bridge <sc19jb@leeds.ac.uk>
2024-07-14 18:13:32 +0100

    binding socket to localmachine

--- httpserver.c
+++ httpserver.c
@@ -1,5 +1,12 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+
 #include <stdio.h>
 #include <WinSock2.h>
+#include <WS2tcpip.h>
 
 int main(void)
 {
@@ -15,11 +22,42 @@ int main(void)
 	{
 
 		printf("Error initialising sockets on windows");
-		return EXIT_FAILURE;
+		return -1;
 
 	}
 
+	SOCKET sock = INVALID_SOCKET;
+	
+	struct sockaddr_in service;
+
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET)
+	{
+
+		printf("Error initialising sockets on windows");
+		WSACleanup();
+		return -1;
+
+	}
+
+	service.sin_family = AF_INET;
+	service.sin_addr.s_addr = inet_addr("127.0.0.1");
+	service.sin_port = htons(27015);
+
+	res = bind(sock, (SOCKADDR *) &service, sizeof(service));
+	if (res != 0)
+	{
+
+		printf("Error initialising sockets on windows");
+		closesocket(sock);
+		WSACleanup();
+		return -1;
+
+	}
+	else
+		printf("Success! socket bound");
+
 	WSACleanup();
 
-	return EXIT_SUCCESS;
+	return -1;
 }
