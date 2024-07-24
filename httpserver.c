8ef06056b7744ee202a84847bdd96aad6089e98f
James Bridge <sc19jb@leeds.ac.uk>
2024-07-24 14:48:57 +0100

    added split function that might not be working, basic html file to send back next

--- httpserver.c
+++ httpserver.c
@@ -5,9 +5,67 @@
 #define WIN32_LEAN_AND_MEAN
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 
+char** split(char* string, char delimitter)
+{
+	// first find number of parts
+	int parts = 1;
+	int index = 0;
+	while (string[index] != '\0' && string[index] != '\r') 
+	{
+		if (string[index] == delimitter)
+			parts++;
+
+		index++;
+	}
+
+	printf("number of splits: %d\n", parts);
+
+	char** res = (char**) malloc(sizeof(char*) * parts);
+
+	int end = 0;
+	int part = 0;
+	int lastPart = 0;
+	while (string[end] != '\0' && string[end - 1] != '\r')
+	{
+
+		if (string[end] == delimitter || string[end] == '\r')
+		{
+
+			int size = end - lastPart;
+			res[part] = (char*) malloc(sizeof(char) * (size + 1));
+
+			int partIndex = 0;
+			for (int i = lastPart; i < end; i++)
+			{
+
+				res[part][partIndex] = string[i];
+				partIndex++;
+
+			}
+			
+			res[part][size] = '\0';
+
+			part++;
+			lastPart = end + 1;
+
+		}
+
+		end++;
+
+	}
+
+	for (int i = 0; i < parts; i++)
+	{
+
+		printf("result of split: %s\n", res[i]);
+
+	}
+
+}
 int main(void)
 {
 
@@ -83,10 +141,9 @@ int main(void)
 
 	}
 
-	printf("Accepting connections\n!");
+	printf("Accepting connections!\n");
 
 	char recvbuf[512];
-
 	res = recv(acceptSocket, recvbuf, 512, 0);
 
 	if (res > 0)
@@ -96,6 +153,22 @@ int main(void)
 	else
 		printf("recv failed: %d\n", WSAGetLastError());
 
+	printf("%s\n", recvbuf);
+
+	int endLine = 0;
+	while (recvbuf[endLine] != '\n') 
+		endLine++;
+
+	char* line = (char*) malloc(sizeof(char) * endLine);
+
+	for (int i = 0; i < endLine; i++)
+		line[i] = recvbuf[i];
+
+	printf("First Line: %s\n", line);
+
+	char** splitLine = split(line, ' ');
+;
+	
 	WSACleanup();
 
 	return -1;
